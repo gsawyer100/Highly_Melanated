a7282ed874353d52ea7ce5068e4bbf69ec654398
Grant Sawyer <gsawyer1234@gmail.com>
2023-06-26 13:50:18 -0400

    Update script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+let button4 = document.getElementById("button4");
+
+
+
+button.addEventListener("click", function() {
+    document.getElementById("button4").innerHTML =  "https://docs.google.com/spreadsheets/d/1_4Ef8sYGWoqljqA1OBielTSJQKJf2iMtHaueHxasL-8/edit#gid=0";
+
+    window.location.href = "https://docs.google.com/spreadsheets/d/1_4Ef8sYGWoqljqA1OBielTSJQKJf2iMtHaueHxasL-8/edit#gid=0";
+  });
+
+let button1 = document.getElementById("button1");
+
+
+button.addEventListener("click", function() {
+    document.getElementById("button1").innerHTML =  "https://docs.google.com/spreadsheets/d/1_4Ef8sYGWoqljqA1OBielTSJQKJf2iMtHaueHxasL-8/edit#gid=0";
+    window.location.href = "https://docs.google.com/spreadsheets/d/1_4Ef8sYGWoqljqA1OBielTSJQKJf2iMtHaueHxasL-8/edit#gid=0";
+  });
+
